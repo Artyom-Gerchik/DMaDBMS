8baabb7557d30752b0f040ad26682f53ffcf5f9e
Artyom-Gerchik <bombruchelyator@gmail.com>
2022-12-01 21:08:18 +0300

    lab5 done

--- LAB5/lab5_procedures.sql
+++ LAB5/lab5_procedures.sql
@@ -1,59 +1,65 @@
--- CREATE OR REPLACE PROCEDURE registerClient(
--- 	login varchar(100),
--- 	password varchar(100),
--- 	first_name varchar(100),
--- 	last_name varchar(100),
--- 	patronymic varchar(100),
--- 	date_of_birth date
--- )
--- LANGUAGE PLPGSQL    
--- AS $$
+----------------------------------------------------------------------------------------------------------------------------------
 
--- DECLARE var uuid;
+CREATE OR REPLACE PROCEDURE registerClient(
+	login varchar(100),
+	password varchar(100),
+	first_name varchar(100),
+	last_name varchar(100),
+	patronymic varchar(100),
+	date_of_birth date
+)
+LANGUAGE PLPGSQL    
+AS $$
 
--- BEGIN
--- 	SELECT gen_random_uuid() INTO var;
+DECLARE var uuid;
+
+BEGIN
+	SELECT gen_random_uuid() INTO var;
     
--- 	INSERT INTO Users VALUES (var,
--- 							  login,
--- 							  password,
--- 							  first_name,
--- 							  last_name,
--- 						      (SELECT id FROM Roles WHERE role_name = 'client'));
+	INSERT INTO Users VALUES (var,
+							  login,
+							  password,
+							  first_name,
+							  last_name,
+						      (SELECT id FROM Roles WHERE role_name = 'client'));
 
--- 	INSERT INTO Clients VALUES (var,
--- 								patronymic,
--- 								date_of_birth);
+	INSERT INTO Clients VALUES (var,
+								patronymic,
+								date_of_birth);
 
---     COMMIT;
--- END;$$
+    COMMIT;
+END;$$
 
 -- CALL registerClient('qwe', 'qwe', 'qwe', 'qwe', 'qwe', '1945-01-01');
 
--- CREATE OR REPLACE PROCEDURE createOrder(
--- 	client_id uuid,
--- 	offer_id uuid,
--- 	final_price float8,
--- 	arriving_date date,
--- 	departure_date date
--- )
--- LANGUAGE PLPGSQL    
--- AS $$
+----------------------------------------------------------------------------------------------------------------------------------
 
--- DECLARE var uuid;
+----------------------------------------------------------------------------------------------------------------------------------
 
--- BEGIN
--- 	SELECT gen_random_uuid() INTO var;
+CREATE OR REPLACE PROCEDURE createOrder(
+	client_id uuid,
+	offer_id uuid,
+	final_price float8,
+	arriving_date date,
+	departure_date date
+)
+LANGUAGE PLPGSQL    
+AS $$
+
+DECLARE var uuid;
+
+BEGIN
+	SELECT gen_random_uuid() INTO var;
     
--- 	INSERT INTO orders VALUES (var,
--- 							  client_id,
--- 							  offer_id,
--- 							  final_price,
--- 							  arriving_date,
--- 							  departure_date);
+	INSERT INTO orders VALUES (var,
+							  client_id,
+							  offer_id,
+							  final_price,
+							  arriving_date,
+							  departure_date);
 
---     COMMIT;
--- END;$$
+    COMMIT;
+END;$$
 
 -- DO $$
 -- DECLARE client_id_for_order uuid;
@@ -68,6 +74,8 @@
 -- 					 '1111-11-11',
 -- 					 '2222-11-11');
 -- END $$;
+
+----------------------------------------------------------------------------------------------------------------------------------
 
 -- SELECT logs.id, logs.client_id , logs.happened_at, logstypes.log_type_name FROM logs
 -- JOIN logstypes ON logs.log_type_id = logstypes.id;
--- LAB5/lab5_triggers.sql
+++ LAB5/lab5_triggers.sql
@@ -1,24 +1,26 @@
 -- DROP TRIGGER order_created ON orders;
 -- DROP FUNCTION log_order_created(); 
 
--- CREATE OR REPLACE FUNCTION log_order_created()
---   RETURNS TRIGGER 
---   LANGUAGE PLPGSQL
---   AS
--- $$
--- BEGIN
--- 		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
--- 		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='orderCreated'), now(), NEW.client_id);
-		 
--- 	RETURN NEW;
--- END;
--- $$
+----------------------------------------------------------------------------------------------------------------------------------
 
--- CREATE TRIGGER order_created
---   BEFORE INSERT
---   ON orders
---   FOR EACH ROW
---   EXECUTE FUNCTION log_order_created();
+CREATE OR REPLACE FUNCTION log_order_created()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
+		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='orderCreated'), now(), NEW.client_id);
+		 
+	RETURN NEW;
+END;
+$$
+
+CREATE TRIGGER order_created
+  BEFORE INSERT
+  ON orders
+  FOR EACH ROW
+  EXECUTE FUNCTION log_order_created();
 
 -- INSERT INTO orders VALUES (gen_random_uuid(),
 -- 						   (SELECT id FROM Clients WHERE patronymic = 'bsuir'),
@@ -27,27 +29,31 @@
 -- 						   '2010-01-01',
 -- 						   '2019-01-11');
 
+----------------------------------------------------------------------------------------------------------------------------------
+
 -- DROP TRIGGER client_registarted ON clients;
 -- DROP FUNCTION log_client_registrated(); 
 
--- CREATE OR REPLACE FUNCTION log_client_registrated()
---   RETURNS TRIGGER 
---   LANGUAGE PLPGSQL
---   AS
--- $$
--- BEGIN
--- 		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
--- 		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='clientRegistrated'), now(), NEW.id);
-		 
--- 	RETURN NEW;
--- END;
--- $$
+----------------------------------------------------------------------------------------------------------------------------------
 
--- CREATE TRIGGER client_registarted
---   BEFORE INSERT
---   ON clients
---   FOR EACH ROW
---   EXECUTE FUNCTION log_client_registrated();
+CREATE OR REPLACE FUNCTION log_client_registrated()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
+		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='clientRegistrated'), now(), NEW.id);
+		 
+	RETURN NEW;
+END;
+$$
+
+CREATE TRIGGER client_registarted
+  BEFORE INSERT
+  ON clients
+  FOR EACH ROW
+  EXECUTE FUNCTION log_client_registrated();
 
 -- INSERT INTO Users VALUES (gen_random_uuid(),
 -- 						  'aab',
@@ -61,33 +67,41 @@
 -- 							'2000-01-01'
 -- 						   );
 
+----------------------------------------------------------------------------------------------------------------------------------
+
+
 -- DROP TRIGGER question_asked ON questions;
 -- DROP FUNCTION log_question_asked(); 
 
--- CREATE OR REPLACE FUNCTION log_question_asked()
---   RETURNS TRIGGER 
---   LANGUAGE PLPGSQL
---   AS
--- $$
--- BEGIN
--- 		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
--- 		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='questionAsked'), now(), NEW.client_id);
-		 
--- 	RETURN NEW;
--- END;
--- $$
+----------------------------------------------------------------------------------------------------------------------------------
 
--- CREATE TRIGGER question_asked
---   BEFORE INSERT
---   ON questions
---   FOR EACH ROW
---   EXECUTE FUNCTION log_question_asked();
+CREATE OR REPLACE FUNCTION log_question_asked()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+		 INSERT INTO logs(id, log_type_id, happened_at, client_id)
+		 VALUES(gen_random_uuid(), (SELECT id FROM logstypes WHERE log_type_name='questionAsked'), now(), NEW.client_id);
+		 
+	RETURN NEW;
+END;
+$$
+
+CREATE TRIGGER question_asked
+  BEFORE INSERT
+  ON questions
+  FOR EACH ROW
+  EXECUTE FUNCTION log_question_asked();
 
 -- INSERT INTO questions VALUES (gen_random_uuid(),
 -- 							 (SELECT id FROM Users WHERE login = 'aa'),
 -- 							 'test_for_trigger',
 -- 							 'test_for_trigger',
 -- 							 'true');
+
+----------------------------------------------------------------------------------------------------------------------------------
+
 
 -- TRUNCATE TABLE logs
 
